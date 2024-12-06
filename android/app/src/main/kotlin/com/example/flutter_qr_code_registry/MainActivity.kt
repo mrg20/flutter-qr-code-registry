b999cef67c48e7549ea40baf8d0aab2534671103
Alba Lamas <alba.v.lamas@gmail.com>
2024-12-06 14:41:05 +0100

    simplify repo and add example

--- android/app/src/main/kotlin/com/example/flutter_qr_code_registry/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_qr_code_registry
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,206 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_zxing/flutter_zxing.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+import 'widgets/debug_info_widget.dart';
+import 'widgets/scan_result_widget.dart';
+import 'widgets/unsupported_platform_widget.dart';
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+void main() {
+  zx.setLogEnabled(kDebugMode);
+  // print zxing version
+  debugPrint('ZXing version:  ${zx.version()}');
+  runApp(const MyApp());
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Zxing Example',
+      debugShowCheckedModeBanner: false,
+      home: DemoPage(),
+    );
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  const DemoPage({super.key});
+
+  @override
+  State<DemoPage> createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  Uint8List? createdCodeBytes;
+
+  Code? result;
+  Codes? multiResult;
+
+  bool isMultiScan = false;
+
+  bool showDebugInfo = true;
+  int successScans = 0;
+  int failedScans = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCameraSupported = defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.android;
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const TabBar(
+            tabs: [
+              Tab(text: 'Scan Code'),
+              Tab(text: 'Create Code'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            if (kIsWeb)
+              const UnsupportedPlatformWidget()
+            else if (!isCameraSupported)
+              const Center(
+                child: Text('Camera not supported on this platform'),
+              )
+            else if (result != null && result?.isValid == true)
+              ScanResultWidget(
+                result: result,
+                onScanAgain: () => setState(() => result = null),
+              )
+            else
+              Stack(
+                children: [
+                  ReaderWidget(
+                    onScan: _onScanSuccess,
+                    onScanFailure: _onScanFailure,
+                    onMultiScan: _onMultiScanSuccess,
+                    onMultiScanFailure: _onMultiScanFailure,
+                    onMultiScanModeChanged: _onMultiScanModeChanged,
+                    onControllerCreated: _onControllerCreated,
+                    isMultiScan: isMultiScan,
+                    scanDelay: Duration(milliseconds: isMultiScan ? 50 : 500),
+                    resolution: ResolutionPreset.high,
+                    lensDirection: CameraLensDirection.back,
+                    flashOnIcon: const Icon(Icons.flash_on),
+                    flashOffIcon: const Icon(Icons.flash_off),
+                    flashAlwaysIcon: const Icon(Icons.flash_on),
+                    flashAutoIcon: const Icon(Icons.flash_auto),
+                    galleryIcon: const Icon(Icons.photo_library),
+                    toggleCameraIcon: const Icon(Icons.switch_camera),
+                    actionButtonsBackgroundBorderRadius:
+                        BorderRadius.circular(10),
+                    actionButtonsBackgroundColor: Colors.black.withOpacity(0.5),
+                  ),
+                  if (showDebugInfo)
+                    DebugInfoWidget(
+                      successScans: successScans,
+                      failedScans: failedScans,
+                      error: isMultiScan ? multiResult?.error : result?.error,
+                      duration: isMultiScan
+                          ? multiResult?.duration ?? 0
+                          : result?.duration ?? 0,
+                      onReset: _onReset,
+                    ),
+                ],
+              ),
+            if (kIsWeb)
+              const UnsupportedPlatformWidget()
+            else
+              ListView(
+                children: [
+                  WriterWidget(
+                    messages: const Messages(
+                      createButton: 'Create Code',
+                    ),
+                    onSuccess: (result, bytes) {
+                      setState(() {
+                        createdCodeBytes = bytes;
+                      });
+                    },
+                    onError: (error) {
+                      _showMessage(context, 'Error: $error');
+                    },
+                  ),
+                  if (createdCodeBytes != null)
+                    Image.memory(createdCodeBytes ?? Uint8List(0), height: 400),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onControllerCreated(_, Exception? error) {
+    if (error != null) {
+      // Handle permission or unknown errors
+      _showMessage(context, 'Error: $error');
+    }
+  }
+
+  _onScanSuccess(Code? code) {
+    setState(() {
+      successScans++;
+      result = code;
+    });
+  }
+
+  _onScanFailure(Code? code) {
+    setState(() {
+      failedScans++;
+      result = code;
+    });
+    if (code?.error?.isNotEmpty == true) {
+      _showMessage(context, 'Error: ${code?.error}');
+    }
+  }
+
+  _onMultiScanSuccess(Codes codes) {
+    setState(() {
+      successScans++;
+      multiResult = codes;
+    });
+  }
+
+  _onMultiScanFailure(Codes result) {
+    setState(() {
+      failedScans++;
+      multiResult = result;
+    });
+    if (result.codes.isNotEmpty == true) {
+      _showMessage(context, 'Error: ${result.codes.first.error}');
+    }
+  }
+
+  _onMultiScanModeChanged(bool isMultiScan) {
+    setState(() {
+      this.isMultiScan = isMultiScan;
+    });
+  }
+
+  _showMessage(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  _onReset() {
+    setState(() {
+      successScans = 0;
+      failedScans = 0;
+    });
+  }
 }
